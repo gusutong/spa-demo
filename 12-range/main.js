e58c914dc59f759649221a591c5ff8632d82403b
gusutong <562656798@qq.com>
2019-04-02 16:36:16 +0800

    H5 built-in controls

---
+++ 12-range/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    var $range = $('input[type="range"]');
+    var $txtAge = $('#age');
+
+    $range.on('input',function(){
+      $txtAge.text($range.val());           
+     });
+});
+
